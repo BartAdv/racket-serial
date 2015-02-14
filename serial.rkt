784479eec3d8065ce7c64fbe920d6bda9ab8b2ad
BartAdv <bprzygoda@gmail.com>
2015-02-14 15:27:13 +0000

    Setup procedure roughly done
    Also, rudimentary open and close helpers

--- serial.rkt
+++ serial.rkt
@@ -3,6 +3,39 @@
 (require termios)
 (require (for-syntax racket/syntax))
 
+(define baudrate-constants
+  (hash 0       B0
+	50      B50     
+	75      B75     
+	110     B110    
+	134     B134    
+	150     B150    
+	200     B200    
+	300     B300    
+	600     B600    
+	1200    B1200   
+	1800    B1800   
+	2400    B2400   
+	4800    B4800   
+	9600    B9600   
+	19200   B19200  
+	38400   B38400  
+	57600   B57600  
+	115200  B115200 
+	230400  B230400 
+	460800  B460800 
+	500000  B500000 
+	576000  B576000 
+	921600  B921600 
+	1000000 B1000000
+	1152000 B1152000
+	1500000 B1500000
+	2000000 B2000000
+	2500000 B2500000
+	3000000 B3000000
+	3500000 B3500000
+	4000000 B4000000))
+
 ;; the setup is based on pyserial: https://github.com/makerbot/pyserial/blob/master/serial/serialposix.py
 (define (setup port
 	       baudrate
@@ -32,7 +65,7 @@
 
   (define vmin (if inter-char-timeout 1 0))
   (define vtime (if inter-char-timeout (* 10 inter-char-timeout) 0))
-  
+
   (flag cflag CLOCAL CREAD)
   (unflag lflag ICANON ECHO ECHOE ECHOK ECHONL (or ECHOCTL 0) (or ECHOKE 0) ISIG IEXTEN)
   (unflag oflag OPOST)
@@ -69,5 +102,28 @@
     (if rtscts
 	(flag cflag CRTSCTS)
 	(unflag cflag CRTSCTS))) ; TODO: CNEW_RTSCTS?
+  
+  (vector-set! (TERMIOS-c_cc t) VMIN vmin)
+  (vector-set! (TERMIOS-c_cc t) VTIME vtime)
+  
+  (set-TERMIOS-c_ispeed! t (hash-ref baudrate-constants baudrate))
+  (set-TERMIOS-c_ospeed! t (hash-ref baudrate-constants baudrate))
+  
   (tcsetattr port TCSANOW t)
-  (TERMIOS-c_cflag t))
+  t)
+
+(define (serial-open path
+		     #:baudrate [baudrate 9600]
+		     #:bytesize [bytesize 8]
+		     #:stopbits [stopbits 'one]
+		     #:parity [parity 'even]
+		     #:xonxoff [xonxoff 0]
+		     #:rtscts [rtscts 0]
+		     #:inter-char-timeout [timeout 3])
+  (let-values ([(in out) (open-input-output-file path #:exists 'append)])
+    (setup in baudrate bytesize stopbits parity xonxoff rtscts timeout)
+    (values in out)))
+
+(define (serial-close in out)
+  (close-input-port in)
+  (close-output-port out))
